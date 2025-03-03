97cb18dec3f8cbf914748f823da60c85d8ac04d9
Alexander Pegot Ogier <alexanderpegoto@gmail.com>
2025-03-03 13:05:22 -0500

    hw1 push

--- hw1/embeddings.py
+++ hw1/embeddings.py
@@ -24,14 +24,14 @@ class Embeddings:
         :param vectors: A 2D array of shape (len(words), embedding_size)
             where for each i, vectors[i] is the embedding for words[i]
         """
-        self.words = list(words)
-        self.indices = {w: i for i, w in enumerate(words)}
-        self.vectors = vectors
+        self.words = list(words) #list of words
+        self.indices = {w: i for i, w in enumerate(words)} # indices to words
+        self.vectors = vectors # intanciates the vectors
 
-    def __len__(self):
+    def __len__(self): #length on instance of embedding
         return len(self.words)
 
-    def __contains__(self, word: str) -> bool:
+    def __contains__(self, word: str) -> bool: # returns if word in list of words
         return word in self.words
 
     def __getitem__(self, words: Iterable[str]) -> np.ndarray:
@@ -41,9 +41,10 @@ class Embeddings:
         :param words: A list of words
         :return: A 2D array of shape (len(words), embedding_size) where
             for each i, the ith row is the embedding for words[i]
-        """
-        raise NotImplementedError("Problem 1b has not been completed yet!")
 
+        """
+        words = [words] if type(words) == str else words
+        return np.array([self.vectors[self.indices[word]] for word in words])
     @classmethod
     def from_file(cls, filename: str) -> "Embeddings":
         """
@@ -53,4 +54,14 @@ class Embeddings:
         :param filename: The name of the file containing the embeddings
         :return: An Embeddings object containing the loaded embeddings
         """
-        raise NotImplementedError("Problem 1b has not been completed yet!")
+        words = []
+        vectors = []
+
+        with open(filename, "r") as file:
+            for line in file:
+                l = line.split()
+                word = l[0]
+                vector = np.array(l[1:], dtype= np.float32)
+                words.append(word)
+                vectors.append(vector)
+        return cls(words, np.array(vectors))
--- hw1/test_analogies.py
+++ hw1/test_analogies.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from embeddings import Embeddings
 
+import re
+
 
 def cosine_sim(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     """
@@ -19,7 +21,14 @@ def cosine_sim(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     :return: An array of shape (m, n), where the entry in row i and
         column j is the cosine similarity between x[i] and y[j]
     """
-    raise NotImplementedError("Problem 3b has not been completed yet!")
+    array = np.zeros((len(x),len(y)))
+    for i in range(len(x)):
+        abs_x = np.linalg.norm(x[i])
+        for j in range(len(y)):
+            abs_y = np.linalg.norm(y[j])
+            value = (x[i] @ y[j]) / (abs_x * abs_y)
+            array[i,j] = value
+    return array
 
 
 def get_closest_words(embeddings: Embeddings, vectors: np.ndarray,
@@ -37,7 +46,18 @@ def get_closest_words(embeddings: Embeddings, vectors: np.ndarray,
         k words that are closest to vectors[i] in the embedding space,
         not necessarily in order
     """
-    raise NotImplementedError("Problem 3c has not been completed yet!")
+    closest_words = []
+    for vector in vectors: # iterating over vector array
+        vecs_list = [] # We need to store the top k words for each vector
+        vector = vector.reshape(1,-1)# Shaping the vector a 2D array for correct computation
+        for e in embeddings.words:
+            cos_sim_val = cosine_sim(vector, embeddings[[e]])
+            vecs_list.append((e, cos_sim_val))
+        # Sorting for top k neighbors
+        top_k = sorted(vecs_list,key=lambda x: x[1], reverse=True)[:k]
+        get_closest_words = [word for word, _ in top_k]
+        closest_words.append(get_closest_words)
+    return closest_words
 
 
 # This type alias represents the format that the testing data should be
@@ -59,8 +79,18 @@ def load_analogies(filename: str) -> AnalogiesDataset:
         format of the data is described in the problem set and in the
         docstring for the AnalogiesDataset type alias
     """
-    raise NotImplementedError("Problem 2b has not been completed yet!")
+    data: AnalogiesDataset = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            match = re.match(r":\s(.+)",line)
+            if match:
+                key = match.group(1)
+                data[key] = []
 
+            else:
+                words = tuple(word.lower() for word in line.split())
+                data[key].append(words)
+    return data
 
 def run_analogy_test(embeddings: Embeddings, test_data: AnalogiesDataset,
                      k: int = 1) -> Dict[str, float]:
@@ -80,4 +110,29 @@ def run_analogy_test(embeddings: Embeddings, test_data: AnalogiesDataset,
         that maps each relation type to the analogy question accuracy
         attained by embeddings on analogies from that relation type
     """
-    raise NotImplementedError("Problem 3d has not been completed yet!")
+    analogy_dict = {}
+
+    # Iterating over test_data analogies
+    for category, analogies in test_data.items():
+        correct = 0
+        total = 0
+        for analogy in analogies:
+            w1,w2,w3,w4 = analogy
+            vecs = embeddings[[w1, w2, w3]]
+
+            # Computing predicted vector
+            predicted_vector = vecs[1:2] - vecs[0:1] + vecs[2:3]
+
+            # Get closest words to the predicted vector
+            closest_words = get_closest_words(embeddings, predicted_vector, k=k)
+
+            # Check if predicted vector == w4
+            if w4 in closest_words[0]:
+                correct += 1
+            total += 1
+
+        # Compute accuracy for each cat
+        accuracy = correct / total
+        analogy_dict[category] = accuracy
+
+    return analogy_dict
